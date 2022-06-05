fdab09a28e7d0392485788124881a2c2eaddd87d
Nisha Dudheliya <nishadudheliya@gmail.com>
2022-06-04 20:53:12 -0400

    First commit

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', home);
+
+function home(req, res, next) {
+  res.render(
+    'index', 
+    { 
+      title: 'Express',
+      userName: 'Nisha'
+    }
+  );
+}
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About'});
+});
+
+/* GET Project page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
+});
+
+/* GET Service page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services'});
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'ContactMe'});
+});
+
+
+module.exports = router;
